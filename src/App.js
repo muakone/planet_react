04d5cc363e71b09cf5dcc190c84a6adc29155cea
muak1 <muak1ayomide@gmail.com>
2021-11-14 22:10:22 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-import { Route, Routes } from 'react-router';
+import { Route, Routes} from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Container, Row } from 'react-bootstrap';
 import NavBARAGAIN from './component/NavBARAGAIN';
 import Card from './component/Card';
 import Character from './component/Character';
 import Navs from './component/Nav';
+
+
 //import SideBar from './component/Sidebar';
 
 function App() {
@@ -22,10 +24,10 @@ function App() {
                   <Navs />
                 </div>
               </div>
-              <Routes>
-                <Route exact path='/' element={<Card />} />
-                <Route exact path='/Character' element={<Character />} />
-              </Routes>
+                      <Routes>
+                        <Route exact path='/' element={<Card />} />
+                        <Route exact path='/Character' element={<Character />} />
+                      </Routes>
             </Row>
           </div>
         </Router>

76981b2a9ec213b8f00078609164b8402ac7d839
muak1 <muak1ayomide@gmail.com>
2021-11-15 12:31:50 +0100

    designed the loader content

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+//ReactDOM.render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>,
+  //document.getElementById('root')
+//);
+const rootElement = document.getElementById("root");
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+  rootElement
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
